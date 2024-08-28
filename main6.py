a60876e1e2da7b8696b0aac5c9895125e325d474
mainor martinez <martimai000@warren.k12.in.us>
2024-08-28 09:44:34 -0400

    initial w&d

---
+++ main6.py
@@ -0,0 +1,4 @@
+name  = input("What is your name: ")
+droids = int(input("how many droids do you want to meet:"))
+wookiees = int(input("How many wookiees do you want to meet:"))
+print(name, "wants to meet", droids, "droids and ", wookiees, "Wookiees!")
